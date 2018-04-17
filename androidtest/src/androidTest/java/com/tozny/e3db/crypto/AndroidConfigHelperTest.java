23a69ee5222e097ea0f544767a37070668db15c3
Lilli Szafranski <lilli@daoofdev.com>
2018-04-17 12:29:10 -0700

    [E3DB-997] Unit test for fsks written and passing.

--- androidtest/src/androidTest/java/com/tozny/e3db/crypto/AndroidConfigHelperTest.java
+++ androidtest/src/androidTest/java/com/tozny/e3db/crypto/AndroidConfigHelperTest.java
@@ -9,8 +9,14 @@ import com.tozny.e3db.Signature;
 import org.junit.Test;
 import org.libsodium.jni.crypto.Random;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Method;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
@@ -18,24 +24,64 @@ import static org.libsodium.jni.Sodium.crypto_sign_bytes;
 import static org.libsodium.jni.Sodium.crypto_sign_publickeybytes;
 
 public class AndroidConfigHelperTest {
-    private final Crypto crypto = new AndroidCrypto();
-    private final Random random = new Random();
-
     private static final String TAG = "AndroidConfigHelperTest";
+
+    private static final String name  = "foo";
+    private static final String loc   = "bar";
+    private static final String alias = "baz";
+
+    private static boolean fileExists(Context context, String privateFile) {
+        File filesDir = context.getFilesDir();
+        return new File(filesDir, privateFile).exists();
+    }
 
     @Test
     public void testFileSystemKeystore() {
         Context context = InstrumentationRegistry.getTargetContext();
 
         try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("foo", Context.MODE_PRIVATE));
-            outputStreamWriter.write("foo");
-            outputStreamWriter.close();
+            if (fileExists(context, name) || fileExists(context, loc)) {
+                File file = new File(context.getFilesDir(), name);
+                file.delete();
+                file = new File(context.getFilesDir(), loc);
+                file.delete();
+            }
+
+            assertFalse(fileExists(context, name));
+            assertFalse(fileExists(context, loc));
+
+            Class<?> clazz = Class.forName("com.tozny.e3db.crypto.FSKSWrapper");
+            Method method = clazz.getDeclaredMethod("getFSKS", Context.class, String.class, String.class);
+            method.setAccessible(true);
+            Object o = method.invoke(null, context, name, loc);
+
+            assertTrue(fileExists(context, name));
+            assertTrue(fileExists(context, loc));
+
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            SecureRandom random = new SecureRandom();
+            keyGen.init(random);
+            SecretKey secretKey = keyGen.generateKey();
+
+            ((KeyStore)o).setEntry(alias, new KeyStore.SecretKeyEntry(secretKey), new KeyStore.PasswordProtection(null));
+
+            method = clazz.getDeclaredMethod("saveFSKS", Context.class, String.class, String.class);
+            method.setAccessible(true);
+            o = method.invoke(null, context, name, loc);
+
+            method = clazz.getDeclaredMethod("getFSKS", Context.class, String.class, String.class);
+            method.setAccessible(true);
+            o = method.invoke(null, context, name, loc);
+
+            assertTrue(((KeyStore)o).containsAlias(alias));
+
+            assertTrue(fileExists(context, name));
+            assertTrue(fileExists(context, loc));
+
         } catch (Exception e) {
-            Log.d(TAG, e.getLocalizedMessage());
+            e.printStackTrace();
+            assertFalse(e.getLocalizedMessage(), true);
         }
-
-
     }
 
     @Test

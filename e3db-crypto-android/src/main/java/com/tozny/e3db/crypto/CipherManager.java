43e33730f1e88ad2e7535c2ed0e61c45d3d0f9b3
Justin Bailey <jgbailey@tozny.com>
2018-04-19 16:34:48 -0700

    Update copyright.

--- e3db-crypto-android/src/main/java/com/tozny/e3db/crypto/CipherManager.java
+++ e3db-crypto-android/src/main/java/com/tozny/e3db/crypto/CipherManager.java
@@ -1,17 +1,24 @@
-package com.tozny.e3db.crypto;
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * 
- * Copyright (c) 2018 
- * 
+/*
+ * TOZNY NON-COMMERCIAL LICENSE
+ *
+ * Tozny dual licenses this product. For commercial use, please contact
+ * info@tozny.com. For non-commercial use, the contents of this file are
+ * subject to the TOZNY NON-COMMERCIAL LICENSE (the "License") which
+ * permits use of the software only by government agencies, schools,
+ * universities, non-profit organizations or individuals on projects that
+ * do not receive external funding other than government research grants
+ * and contracts.  Any other use requires a commercial license. You may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://tozny.com/legal/non-commercial-license.
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License. Portions of the software are Copyright (c) TOZNY LLC, 2018.
  * All rights reserved.
- * 
- * e3db-java
- * 
- * Created by Lilli Szafranski on 4/9/18.
- * 
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ *
+ */
 
+package com.tozny.e3db.crypto;
 
 import android.content.Context;
 import android.os.Build;
